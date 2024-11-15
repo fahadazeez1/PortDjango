a932b4c9a0ad0bbbd542a9fc6bba426b8dd19751
fahadazeez1 <azeezfahad4@gmail.com>
2024-11-15 16:44:06 +0530

    UI and other process

--- portDjango/myapp/views.py
+++ portDjango/myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def first(request):
+    if request.method=='POST':
+        nm=request.POST.get('name')
+        print(nm)    
+        # return HttpResponse(f"Email received: {nm}")
     return render(request,'index.html')
